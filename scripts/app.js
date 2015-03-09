60df52b97cc90447ba52eb421baf3cab7a80c24a
Paul Dilyard <paul.dilyard@gmail.com>
2015-03-08 21:00:10 -0400

    Disable HTML5 mode

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,5 @@ angular.module('khe', ['ngRoute', 'ngCookies', 'btford.socket-io']);
 angular
   .module('khe')
   .config(['$locationProvider', function ($locationProvider) {
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(false);
   }]);
