af2e53cc8e1111d067c4d11da5fe54ca67a5d0cf
Paul Dilyard <paul.dilyard@gmail.com>
2015-03-24 17:19:56 -0400

    Refactor attendee controller

--- scripts/controllers/attendees.js
+++ scripts/controllers/attendees.js
@@ -8,284 +8,317 @@ angular
   }])
   .controller('StaffAttendeesCtrl', ['User', 'Application', function (User, Application) {
 
-    var self = this;
-    var applicationModel = new Application();
-    var userModel = new User();
-    var socket = userModel.socket();
-    self.user = userModel.getMe();
+    /**
+    * The interface that a template can use. To see all variables available
+    * to the template, just console.log(view) at the bottom of this controller.
+    */
+    var view = this;
 
-    // All users
-    self.users = [];
-    // Currently displayed users
-    self.current = [];
+    var Models = {
+      user: new User(),
+      application: new Application()
+    };
 
-    // The method name of the current filter on the users
-    // This is used to re-run any filters when the user-list updates
-    // It can also be used to see what the current list of users is
-    self.currentFilter = 'all';
+    /**
+    * The currently logged in user
+    */
+    view.me = Models.user.getMe();
 
-    // Get all users
-    applicationModel.list().
-    success(function (data) {
-      self.users = data.users;
-      readable();
-      self.current = self.users;
-      filterUsers();
-    }).
-    error(function (data) {
-      self.errors = data.errors || ['An internal error has occurred'];
-    });
+    /**
+    * Any errors to display
+    */
+    view.errors = [];
 
-    // Listen for new users
-    socket.on('create', function (user) {
-      self.users.push(user);
-      readable();
-      filterUsers();
-      self[self.currentFilter]();
-    });
+    /**
+    * An array of the currently displayed users
+    */
+    view.users = [];
 
-    // Listen for updated users
-    socket.on('update', function (user) {
-      self.users = self.users.map(function (u) {
-        if (u._id == user._id) {
-          u.email = user.email;
-        }
-        return u;
+    /**
+    * Arrays of different types of users
+    */
+    view.all = [];
+    view.applied = [];
+    view.going = [];
+    view.approved = [];
+    view.waitlisted = [];
+    view.pending = [];
+    view.denied = [];
+    view.travel = [];
+    view.checked = [];
+
+    /**
+    * Get a list of all users with applications
+    */
+    var get = function () {
+      Models.application.list().
+      success(function (data) {
+        view.all = data.users;
+        reload();
+      }).
+      error(function (data) {
+        view.errors = data.errors || ['An internal error has occurred'];
       });
-      readable();
-      filterUsers();
-      self[self.currentFilter]();
-    });
+    };
 
-    // Listen for deleted users
-    socket.on('delete', function (user) {
-      self.users = self.users.filter(function (u) {
-        return u._id != user._id;
+    /**
+    * Set up socket listeners
+    */
+    var listen = function () {
+      var socket = Models.user.socket();
+
+      socket.on('create', function (user) {
+        view.all.push(user);
+        reload();
       });
-      readable();
-      filterUsers();
-      self[self.currentFilter]();
-    });
 
-    // Display all users
-    self.all = function () {
-      self.currentFilter = 'all';
-      self.current = self.users;
+      socket.on('update', function (user) {
+        view.all = view.all.map(function (u) {
+          if (u._id == user._id) {
+            u.email = user.email;
+          }
+          return u;
+        });
+        reload();
+      });
+
+      socket.on('delete', function (user) {
+        view.all = view.all.filter(function (u) {
+          return u._id != user._id;
+        });
+        reload();
+      });
     };
 
-    // Display users with submitted applications
-    self.showApplied = function () {
-      self.currentFilter = 'showApplied';
-      self.current = self.applied;
+    /**
+    * Trigger reload of the user list
+    * Call this function each time the list of users changes somehow
+    */
+    var reload = function () {
+      view.filter.init();
+      view.filter.apply(view.filter.current);
     };
 
-    // Display users that have RSVPd yes
-    self.showGoing = function () {
-      self.currentFilter = 'showGoing';
-      self.current = self.going;
-    };
+    /**
+    * Structure to filter users
+    */
+    view.filter = {
 
-    // Display approved users
-    self.showApproved = function () {
-      self.currentFilter = 'showApproved';
-      self.current = self.approved;
-    };
+      /**
+      * The currently applied filter
+      */
+      current: 'all',
 
-    // Display waitlisted users
-    self.showWaitlisted = function () {
-      self.currentFilter = 'showWaitlisted';
-      self.current = self.waitlisted;
-    };
+      /**
+      * Initialize every array with the different types of users
+      */
+      init: function () {
+        view.applied = view.all.filter(function (user) {
+          return user.application;
+        });
 
-    // Display pending users
-    self.showPending = function () {
-      self.currentFilter = 'showPending';
-      self.current = self.pending;
-    };
+        view.going = view.all.filter(function (user) {
+          return user.application && user.application.going;
+        });
 
-    // Display denied users
-    self.showDenied = function () {
-      self.currentFilter = 'showDenied';
-      self.current = self.denied;
-    };
+        view.approved = view.all.filter(function (user) {
+          return user.application && user.application.status == 'approved';
+        });
 
-    // Display users that have requested travel
-    self.showTravel = function () {
-      self.currentFilter = 'showDenied';
-      self.current = self.travel;
-    };
+        view.waitlisted = view.all.filter(function (user) {
+          return user.application && user.application.status == 'waitlisted';
+        });
 
-    // Display only checked in users
-    self.showChecked = function () {
-      self.currentFilter = 'showChecked';
-      self.current = self.checked;
-    }
+        view.pending = view.all.filter(function (user) {
+          return user.application && user.application.status == 'pending';
+        });
 
-    // Expand a user
-    self.toggle = function (user) {
-      if (self.expandedId == user._id) {
-        self.expandedId = '';
-      } else {
-        self.expandedId = user._id;
+        view.denied = view.all.filter(function (user) {
+          return user.application && user.application.status == 'denied';
+        });
+
+        view.travel = view.all.filter(function (user) {
+          return user.application && user.application.travel;
+        });
+
+        view.checked = view.all.filter(function (user) {
+          return user.application && user.application.checked;
+        });
+      },
+
+      /**
+      * Apply a filter to the list of current users
+      * @param filter The name of the filter to apply
+      */
+      apply: function (filter) {
+        this.current = filter;
+        switch (filter) {
+          case 'all':
+            view.users = view.all;
+            break;
+          case 'applied':
+            view.users = view.applied;
+            break;
+          case 'going':
+            view.users = view.going;
+            break;
+          case 'approved':
+            view.users = view.approved;
+            break;
+          case 'waitlisted':
+            view.users = view.waitlisted;
+            break;
+          case 'pending':
+            view.users = view.pending;
+            break;
+          case 'denied':
+            view.users = view.denied;
+            break;
+          case 'travel':
+            view.users = view.travel;
+            break;
+          case 'checked':
+            view.users = view.checked;
+            break;
+        };
       }
+
     };
 
-    // Check if a user is expanded
-    self.expanded = function (user) {
-      return self.expandedId == user._id;
-    };
+    view.visibility = {
 
-    // Edit the status of a user
-    self.editStatus = function (user) {
-      user.application.oldStatus = user.application.status;
-      user.editingStatus = true;
-    };
-
-    // Save the status of a user
-    self.saveStatus = function (user) {
-      applicationModel.updateById(user._id, {
-        status: user.application.status
-      }).
-      success(function (data) {
-        user.editingStatus = false;
-      }).
-      error(function (data) {
-        self.errors = data.errors || ['An internal error occurred'];
-      });
-    };
-
-    // Cancel editing the status
-    self.cancelEditStatus = function (user) {
-      user.application.status = user.application.oldStatus;
-      user.editingStatus = false;
-    };
-
-    // Edit checked in status of a user
-    self.editChecked = function (user) {
-      user.application.oldChecked = user.application.checked;
-      user.editingChecked = true;
-    };
-
-    // Save the checked in status of a user
-    self.saveChecked = function (user) {
-      var checked = (user.application.checked == 'Yes' ? true : false);
-      applicationModel.updateById(user._id, {
-        checked: checked
-      }).
-      success(function (data) {
-        user.editingChecked = false;
-      }).
-      error(function (data) {
-        self.errors = data.errors || ['An internal error occurred'];
-      });
-    };
-
-    // Cancel editing the checked in status of a user
-    self.cancelEditChecked = function (user) {
-      user.application.checked = user.application.oldChecked;
-      user.editingChecked = false;
-    };
-
-    // Edit the role of a user
-    self.editRole = function (user) {
-      user.oldRole = user.role;
-      user.editingRole = true;
-    };
-
-    // Save the role of a user
-    self.saveRole = function (user) {
-      userModel.updateById(user._id, {role: user.role}).
-      success(function (data) {
-        user.editingRole = false;
-      }).
-      error(function (data) {
-        self.errors = data.errors || ['An internal error occurred'];
-      });
-    };
-
-    // Cancel editing the role
-    self.cancelEditRole = function (user) {
-      user.role = user.oldRole;
-      user.editingRole = false;
-    };
-
-    // Create arrays of users that fit into each category
-    // This way, changing filters is a matter of assigning variables
-    function filterUsers() {
-      self.applied = self.users.filter(function (user) {
-        return user.application;
-      });
-
-      self.going = self.users.filter(function (user) {
-        return user.application && user.application.going == 'Going';
-      });
-
-      self.approved = self.users.filter(function (user) {
-        return user.application && user.application.status == 'approved';
-      });
-
-      self.waitlisted = self.users.filter(function (user) {
-        return user.application && user.application.status == 'waitlisted';
-      });
-
-      self.pending = self.users.filter(function (user) {
-        return user.application && user.application.status == 'pending';
-      });
-
-      self.denied = self.users.filter(function (user) {
-        return user.application && user.application.status == 'denied';
-      });
-
-      self.travel = self.users.filter(function (user) {
-        return user.application && user.application.travel == 'Yes';
-      });
-
-      self.checked = self.users.filter(function (user) {
-        return user.application && user.application.checked == 'Yes';
-      });
-    }
-
-    // Make the data human-readable
-    // Possibly move this into filters? It's pretty inefficient currently
-    function readable() {
-      for (var i = 0; i < self.users.length; i++) {
-        var user = self.users[i];
-        if (user.application) {
-          switch (user.application.shirt) {
-            case 'S':
-              user.application.shirt = 'Small';
-              break;
-            case 'M':
-              user.application.shirt = 'Medium';
-              break;
-            case 'L':
-              user.application.shirt = 'Large';
-              break;
-            case 'XL':
-              user.application.shirt = 'X-Large';
-              break;
-          }
-          if (!user.application.gender) user.application.gender = '-';
-          user.application.first = (user.application.first ? 'Yes' : 'No');
-          if (user.application.dietary.length) {
-            var diet = user.application.dietary[0];
-            for (var j = 1; j < user.application.dietary.length; j++) {
-              diet = diet + ', ' + user.application.dietary[j];
-            }
-            user.application.dietary = diet;
-          } else {
-            user.application.dietary = 'None';
-          }
-          user.application.travel = (user.application.travel ? 'Yes' : 'No');
-          user.application.checked = (user.application.checked ? 'Yes': 'No');
-          if (user.application.going === undefined) {
-            user.application.going = 'None';
-          } else if (user.application.going === false) {
-            user.application.going = 'Not Going';
-          } else {
-            user.application.going = 'Going';
-          }
+      /**
+      * Toggle the expanded view of a given user
+      */
+      toggle: function (user) {
+        if (this.expandedId == user._id) {
+          this.expandedId = '';
+        } else {
+          this.expandedId = user._id;
         }
-      };
+      },
+
+      /**
+      * Check to see whether a user is expanded
+      */
+      check: function (user) {
+        return this.expandedId == user._id;
+      }
+
     };
+
+    view.status = {
+
+      /**
+      * Edit the status of a user
+      */
+      edit: function (user) {
+        user.application.oldStatus = user.application.status;
+        user.editingStatus = true;
+      },
+
+      /**
+      * Save the status of a user
+      */
+      save: function (user) {
+        Models.application.updateById(user._id, {
+          status: user.application.status
+        }).
+        success(function (data) {
+          user.editingStatus = false;
+          reload();
+        }).
+        error(function (data) {
+          errors = data.errors || ['An internal error occurred'];
+        });
+      },
+
+      /**
+      * Cancel editing the status of a user
+      */
+      cancel: function (user) {
+        user.application.status = user.application.oldStatus;
+        user.editingStatus = false;
+      }
+
+    };
+
+    view.checkedIn = {
+
+      /**
+      * Edit the checked in status of a user
+      */
+      edit: function (user) {
+        user.application.oldChecked = user.application.checked;
+        user.editingChecked = true;
+      },
+
+      /**
+      * Save the checked-in status of a user
+      */
+      save: function (user) {
+        Models.application.updateById(user._id, {
+          checked: user.application.checked
+        }).
+        success(function (data) {
+          user.editingChecked = false;
+          reload();
+        }).
+        error(function (data) {
+          errors = data.errors || ['An internal error occurred'];
+        });
+      },
+
+      /**
+      * Cancel editing the checked-in status of a user
+      */
+      cancel: function (user) {
+        user.application.checked = user.application.oldChecked;
+        user.editingChecked = false;
+      }
+
+    };
+
+    view.role = {
+
+      /**
+      * Edit the role of a user
+      */
+      edit: function (user) {
+        user.oldRole = user.role;
+        user.editingRole = true;
+      },
+
+      /**
+      * Save the role of a user
+      */
+      save: function (user) {
+        Models.user.updateById(user._id, {
+          role: user.role
+        }).
+        success(function (data) {
+          user.editingRole = false;
+          reload();
+        }).
+        error(function (data) {
+          errors = data.errors || ['An internal error occurred'];
+        });
+      },
+
+      /**
+      * Cancel editing the role
+      */
+      cancel: function (user) {
+        user.role = user.oldRole;
+        user.editingRole = false;
+      }
+
+    };
+
+    /**
+    * Initialize the controller
+    */
+    get();
+    listen();
+
   }]);
