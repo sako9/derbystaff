0d1810f55e5983093f6e31e7824cabb839373f39
Paul Dilyard <paul.dilyard@gmail.com>
2015-04-02 16:35:36 -0400

    Add place for client ID in config

--- config/config_example.js
+++ config/config_example.js
@@ -1,6 +1,9 @@
 var config = {
 
   // The API base url *without* a trailing slash
-  api: 'http://localhost:3000'
+  api: 'http://localhost:3000',
+
+  // A valid client ID
+  client: 'abc123'
 
 };
