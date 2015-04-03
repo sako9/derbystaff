f005914d79e370d5d70369c40c2caf804c2ca5de
Paul Dilyard <paul.dilyard@gmail.com>
2015-04-03 17:26:37 -0400

    Special nginx conf for vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -9,9 +9,17 @@ Vagrant.configure(2) do |config|
   config.vm.synced_folder "./", "/var/www/kenthackenough-ui-staff"
 
   # Forward port
-  config.vm.network "forwarded_port", guest: 80, host: 3000
+  config.vm.network "forwarded_port", guest: 80, host: 3001
+  config.vm.network "forwarded_port", guest: 22, host: 2323, id: "ssh"
 
   # Provision
-  config.vm.provision "shell", path: "provision.sh"
+  config.vm.provision "shell", path: "provision.sh" do |s|
+    s.args = ["dev"]
+  end
+
+  # Make symlinks work on Windows
+  config.vm.provider "virtualbox" do |v|
+    v.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root", "1"]
+  end
 
 end
--- provision.sh
+++ provision.sh
@@ -1,5 +1,6 @@
 # install nginx
 apt-get update
+apt-get upgrade -y
 apt-get install -y nginx
 
 # set up nginx
@@ -7,10 +8,12 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
   rm /etc/nginx/sites-enabled/default
   rm /etc/nginx/sites-available/default
 fi
-cp /var/www/kenthackenough-ui-staff/config/staff.khe.conf /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/staff.khe.conf /etc/nginx/sites-enabled
+if [ $1 == "dev" ]; then
+  cp /var/www/kenthackenough-ui-staff/config/dev.conf /etc/nginx/sites-available
+  ln -s /etc/nginx/sites-available/dev.conf /etc/nginx/sites-enabled
+fi
 service nginx reload
 
 echo "--------------------------------------------"
-echo "|  KHE Staff up at http://localhost:3000/  |"
+echo "|  KHE Staff up at http://localhost:3001/  |"
 echo "--------------------------------------------"
