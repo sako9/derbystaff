b49900be0b7e120c7f1ada7f1037878aefbc8ae4
Paul Dilyard <paul.dilyard@gmail.com>
2015-04-04 13:00:31 -0400

    Messages cannot come from the future

--- scripts/filters/relativeDate.js
+++ scripts/filters/relativeDate.js
@@ -2,6 +2,9 @@ angular
   .module('khe')
   .filter('relativeDate', function () {
     return function (input) {
+      if (new Date(input).getTime() > Date.now()) {
+        return 'just now';
+      }
       return moment(input).fromNow();
     };
   });
