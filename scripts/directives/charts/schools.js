28d447858c0c693540716817256d4a6587699580
Paul Dilyard <paul.dilyard@gmail.com>
2015-07-21 00:54:06 -0500

    Revert "Easier visualization of schools"
    This reverts commit 675f0df55d3ead2cb126a5a2e5192e38d71a66a8.

--- scripts/directives/charts/schools.js
+++ scripts/directives/charts/schools.js
@@ -16,13 +16,39 @@ angular
         Models.stats.schools().
         success(function (data) {
 
-          scope.schools = data.schools;
-
           data.schools.sort(function (a, b) {
             if (a.count > b.count) return -1;
             if (a.count < b.count) return 1;
             return 0;
           });
+
+          var labels = data.schools.map(function (r) {
+            return r.name;
+          });
+
+          var numbers = data.schools.map(function (r) {
+            return r.count;
+          });
+
+          new Chartist.Bar('#schools.ct-chart', {
+            labels: labels,
+            series: [numbers]
+          }, {
+            seriesBarDistance: 10,
+            reverseData: true,
+            horizontalBars: true,
+            axisY: {
+              offset: 150,
+              showGrid: false
+            },
+            axisX: {
+              labelInterpolationFnc: function(value) {
+                if (value % 1 === 0) return value;
+              }
+            }
+          });
+
+          $("#schools.ct-chart").height(data.schools.length * 30);
 
         });
 
--- scripts/directives/charts/shirts.js
+++ scripts/directives/charts/shirts.js
@@ -42,7 +42,7 @@ angular
             }
           });
 
-          $("#shirts.ct-chart").height(4 * 35);
+          $("#shirts.ct-chart").height(4 * 30);
 
         });
 
